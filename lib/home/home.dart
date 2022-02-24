1915b74c743be7c11be2143d323a438593e781ed
Ararchy01 <ararchy0621@gmail.com>
2022-02-23 17:53:59 -0800

    Bottom bar try

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -27,6 +27,7 @@ class _HomeState extends State<Home> {
     FirebaseStorage storage = FirebaseStorage.instance;
     try {
       await storage.ref().putFile(file);
+      print(storage.ref());
     } catch (e) {
       print(e);
     }
@@ -37,65 +38,72 @@ class _HomeState extends State<Home> {
     return ChangeNotifierProvider<HomeStreamModel>(
       create: (_) => HomeStreamModel()..fetchDogs(),
       child: Scaffold(
-        appBar: AppBar(
-          title: const Icon(Icons.pets),
-        ),
-        body: Center(
-          child: Consumer<HomeStreamModel>(builder: (context, model, child) {
-            final List<Dog>? dogs = model.dogs;
-            if (dogs == null) {
-              return const CircularProgressIndicator();
-            }
-            return GridView.count(
-                crossAxisCount: 2,
-                crossAxisSpacing: 20,
-                mainAxisSpacing: 250,
-                scrollDirection: Axis.vertical,
-                children: dogs
-                    .map((dog) => Column(
-                          children: [
-                            Image.network(
-                                'https://rosevalleywhiteshepherds.net/wp-content/uploads/2020/09/Bolt-Grand-Prairie-1024x1024.jpg',
-                            height: 50),
-                            Text(dog.name,
-                                style: TextStyle(color: Colors.blueAccent)),
-                            Text(dog.walkers.join('\n')),
-                            FloatingActionButton(
-                                onPressed: _upload,
-                                child: Icon(Icons.upload_file_outlined))
-                          ],
-                        ))
-                    .toList());
-          }),
-        ),
-        floatingActionButton: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            FloatingActionButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const AddDog(),
-                        fullscreenDialog: true));
-              },
-              tooltip: 'Add Dog',
-              child: const Icon(Icons.add),
-            ),
-            FloatingActionButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const SignIn(),
-                        fullscreenDialog: true));
-              },
-              tooltip: 'SignIn',
-              child: const Icon(Icons.accessible_outlined),
-            ),
-          ],
-        )
-      ),
+          appBar: AppBar(
+            title: const Icon(Icons.pets),
+          ),
+          body: Center(
+            child: Consumer<HomeStreamModel>(builder: (context, model, child) {
+              final List<Dog>? dogs = model.dogs;
+              if (dogs == null) {
+                return const CircularProgressIndicator();
+              }
+              return GridView.count(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 20,
+                  mainAxisSpacing: 250,
+                  scrollDirection: Axis.vertical,
+                  children: dogs
+                      .map((dog) => Column(
+                            children: [
+                              Image.network(
+                                  'https://rosevalleywhiteshepherds.net/wp-content/uploads/2020/09/Bolt-Grand-Prairie-1024x1024.jpg',
+                                  height: 50),
+                              Text(dog.name,
+                                  style: TextStyle(color: Colors.blueAccent)),
+                              Text(dog.walkers.join('\n')),
+                              FloatingActionButton(
+                                  onPressed: _upload,
+                                  child: Icon(Icons.upload_file_outlined))
+                            ],
+                          ))
+                      .toList());
+            }),
+          ),
+          //TODO
+          bottomNavigationBar: BottomNavigationBar(
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.pets), label: 'Pet'),
+              BottomNavigationBarItem(icon: Icon(Icons.login), label: 'Login'),
+              BottomNavigationBarItem(icon: Icon(Icons.logout), label: 'Logout')
+            ],
+          ),
+          floatingActionButton: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              FloatingActionButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const AddDog(),
+                          fullscreenDialog: true));
+                },
+                tooltip: 'Add Dog',
+                child: const Icon(Icons.add),
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const SignIn(),
+                          fullscreenDialog: true));
+                },
+                tooltip: 'SignIn',
+                child: const Icon(Icons.login),
+              ),
+            ],
+          )),
     );
   }
 }
